a41ac6c83d32652a9c5979bee3b6b0009711e066
Rizwan1056 <rizwi1056@gmail.com>
2024-06-28 00:28:06 +0500

    finally done

--- index.ts
+++ index.ts
@@ -1,1 +1,41 @@
-console.log("hi");
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+const currency: any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name:`from`,
+            message:`Enter from currency`,
+            type:`list`,
+            choices:[`USD`,`EUR`,`GBP`,`INR`,`PKR`]
+
+        },
+        {
+            name:`to`,
+            message:`Enter to currency`,
+            type:`list`,
+            choices:[`USD`,`EUR`,`GBP`,`INR`,`PKR`]   
+        },
+        {
+            name:`amount`,
+            message:` Plz Enter Your Amount`,
+            type:`number`
+        }
+    ]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount/fromAmount
+let convertedAmount= baseAmount*toAmount
+console.log(convertedAmount.toFixed(1));
+
+
